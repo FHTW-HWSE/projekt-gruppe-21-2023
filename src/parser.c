c427959d54b33b8f5942496fbffe33cc00ffc3f9
Johannes Seitz <ic22b086@technikum-wien.at>
2023-04-15 11:28:47 +0200

    unnötige Zeilen in parser auskommentiert muss anders gebaut werde

---
+++ src/parser.c
@@ -0,0 +1,20 @@
+// TODO Header ins Include File brinngen und aufräumen
+
+#include main.h
+#include parser.h
+
+int parsePerson(char *text, struct person *toPerson)
+{
+    char readString[50];
+    int i = 0;
+
+    /* do{
+
+        do{
+            toPerson->n
+
+        }while(text[i] != ',');
+
+    } while (text[i] != EOF); // oder bis text[i] != '/0' je nach dem wies dann im File lesen besser passt
+    */
+}
