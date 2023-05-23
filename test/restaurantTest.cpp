6ce4ad2cac6120f969bc8f74b244c97da3ba86c1
PetarGol <petargo12@gmail.com>
2023-05-23 19:56:38 +0200

    Add the list of neccessary Unit-Tests

---
+++ test/restaurantTest.cpp
@@ -0,0 +1,94 @@
+#include <catch2/catch.hpp>
+
+SCENARIO("CreatingRestaurantWithPositiveParameter")
+{
+    GIVEN("A User sets the size of the restaurant")
+    {
+
+    }    
+    WHEN("The parameters are correct")
+    {
+
+    }
+    THEN("The size of the restaurant is created")
+    {
+
+    }
+}
+SCENARIO("CreatingRestaurantWithNegativeCoordinates")
+{
+   GIVEN("A User sets the size of the restaurant")
+    {
+
+    }
+    WHEN("The parameters are negative.")
+    {
+      
+
+    }
+    THEN("A Errorwaning will be sent.")
+    {
+      
+    }
+}
+SCENARIO("CreateATableInTheRestaurant")
+{
+  GIVEN("User wants create a Table")
+  {
+
+  }
+  WHEN("Table is in the restaurant")
+  {
+
+  }
+  THEN("A table is created in the restaurant")
+  {
+
+  }
+}
+SCENARIO("CreateTableOutsideOfTheRestaurantWithNegativeParamter")
+{
+ GIVEN("User wants create a Table")
+  {
+
+  }
+  WHEN("Table is outside of the restaurant")
+  {
+
+  }
+  THEN("User gets an Error-Message")
+  {
+
+  } 
+}
+
+SCENARIO("CreateTableOnAnExistingTable")
+{
+  GIVEN("User wants create a Table")
+  {
+
+  }
+  WHEN("There is already a table")
+  {
+
+  }
+  THEN("User gets an Error-Message")
+  {
+
+  } 
+}
+SCENARIO("CreateTableOutsideOfTheRestaurantWithPostiveParamter")
+{
+ GIVEN("User wants create a Table")
+  {
+
+  }
+  WHEN("Table is outside of the restaurant")
+  {
+
+  }
+  THEN("User gets an Error-Message")
+  {
+
+  } 
+}
