03999db7861f1c735d291dbad9360a5f442f0652
Johannes Seitz <ic22b086@technikum-wien.at>
2023-06-06 20:40:39 +0200

    start Contact Tracing - ToDo Zeiten mit Stringcompare vergleichen

--- include/contacTracing.h
+++ include/contacTracing.h
@@ -5,4 +5,4 @@
 #include "structhandeling.h"
 
 struct booking *findBookingfromPerson (struct booking toSearch, struct person toFind);
-int *findContact(int *returnArray, struct booking startBooking, struct booking findContact, struct person toFind);
+int *findContact(int *returnArray, struct booking *startBooking, struct booking *findContact, struct person *toFind, struct person *startPerson);
--- src/contactTracing.c
+++ src/contactTracing.c
@@ -5,7 +5,7 @@
 #include "contact.h"
 
 
-booking findBookingfromPerson(struct booking toSearch, struct person toFind){
+struct booking *findBookingfromPerson(struct booking toSearch, struct person toFind){
     while(toSearch->next != NULL){
         if(toFind->id == toSeach->idPerson){
 
@@ -17,3 +17,14 @@ booking findBookingfromPerson(struct booking toSearch, struct person toFind){
     return toSeach;
 
 }
+
+int *findContact(int *returnArray, struct booking *startBooking, struct booking *findContact, struct person *toFind, struct person *startPerson){
+    while(startBooking->next != NULL){
+        if(findContact->starttime > startBooking->endtime || findContact->endtime < startBooking->starttime)  // toDo mit Stingcompare machen
+
+
+
+    }
+
+    return returnArray;
+}   
