4fb5fe4b4f5cc0f2605bb443181b7a47dc19777a
PetarGol <petargo12@gmail.com>
2023-06-06 19:50:43 +0200

    test

--- src/contact.c
+++ src/contact.c
@@ -1,12 +1,5 @@
 #include <stdio.h>;
 #include <stdlib.h>;
 #include "../include/parser.h";
-#include "../include/main.h";
-struct person 
-{
- char name [50];
- char telefonnumber [32];
- int id;
-};
-
+#include "../include/contact.h";
 
