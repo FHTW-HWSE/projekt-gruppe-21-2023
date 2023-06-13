ca1ec2acdf3fb07954c97252e51f3c0a4704a94d
PetarGol <petargo12@gmail.com>
2023-06-13 19:33:53 +0200

    Unit Part 2

---
+++ test/contactTest.cpp
@@ -0,0 +1,16 @@
+#include <catch2/catch.hpp>
+#include "../include/contact.h"
+
+SCENARIO("AddContactWithCorrctParamter"){
+    GIVEN("User wants to add a Contact"){
+        WHEN("the Parameter is incorrect"){
+            THEN("User will be added to contact list"){
+                
+            }
+        }
+   }
+}
+SCENARIO("AddConctactWith")
+{
+
+}
